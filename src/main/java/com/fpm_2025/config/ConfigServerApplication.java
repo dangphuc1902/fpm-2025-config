20eb6f11c788d77595000abe05d458dd47a9d6ea
Phuc Dang <phuctrong1902@gmail.com>
2025-09-11 06:02:23 +0700

    Init project config

---
+++ src/main/java/com/fpm_2025/config/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.fpm_2025.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
